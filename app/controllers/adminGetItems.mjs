795cb52c71bdecd27353d378ec92b890851219bb
akhilesh423 <akhilakhilesh423@gmail.com>
2024-03-09 23:46:49 +0530

    pagination

--- app/controllers/adminGetItems.mjs
+++ app/controllers/adminGetItems.mjs
@@ -2,7 +2,17 @@ import FoodModel from '../models/foodModel.mjs';
 
 const adminGetItems = async (req, res) => {
   try {
-    const allItems = await FoodModel.find();
+    // Get the page number from the query parameters
+    const pageNumber = parseInt(req.query.page) || 1;
+    const pageSize = 15; // Define the number of items per page
+
+    // Calculate the skip value to determine the starting index of items to retrieve
+    const skip = (pageNumber - 1) * pageSize;
+
+    // Fetch items from the database with pagination
+    const allItems = await FoodModel.find().skip(skip).limit(pageSize);
+
+    // Send the paginated items back to the client
     res.status(200).send(allItems);
   } catch (err) {
     console.error('Error fetching items:', err);
